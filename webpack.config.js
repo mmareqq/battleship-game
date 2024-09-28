4a2b73abe35ff8b9176fb7ba2d814cf9c583ec0f
mmareqq <szlejter.marek007@gmail.com>
2024-09-28 22:24:30 +0200

    Reduce unnecessary plugins
    Commented out unnecessary plugins for development mode to reduce build time and increase efficiency

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin")
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+// const CopyPlugin = require("copy-webpack-plugin")
+// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   mode: "development",
   entry: './src/index.js',
@@ -11,7 +11,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
-  devtool: "eval-source-map",
+  devtool: "eval",
   devServer: {
     static: {
       directory: path.join(__dirname, 'src')
@@ -25,35 +25,35 @@ module.exports = {
     hints: false
   },
 
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin({
-        terserOptions: {
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-      // Other minimizers if needed
-    ],
-  },
+  // optimization: {
+  //   minimize: true,
+  //   minimizer: [
+  //     new CssMinimizerPlugin(),
+  //     new TerserPlugin({
+  //       terserOptions: {
+  //         format: {
+  //           comments: false,
+  //         },
+  //       },
+  //       extractComments: false,
+  //     }),
+  //     // Other minimizers if needed
+  //   ],
+  // },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html', // your HTML template file
     }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: './src/styles', to: "styles"
-        },
-        {
-          from: './src/assets', to: "assets"
-        }
-      ]
-    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: './src/styles', to: "styles"
+    //     },
+    //     {
+    //       from: './src/assets', to: "assets"
+    //     }
+    //   ]
+    // }),
   ],
 
   module: {
