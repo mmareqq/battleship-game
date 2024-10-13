a58467386dfcf76a503f0a8fd19864fbc26305bb
mmareqq <szlejter.marek007@gmail.com>
2024-10-13 15:06:38 +0200

    Delete tests

--- tests/boardCreator.test.js
+++
@@ -1,6 +0,0 @@
-const checkPlaceForShip = require('../src/modules/placeShip.js')
-
-it('Board creator doesnt let you place ship going outside board', () => {
-   expect(checkPlaceForShip(8, 0, 5, true)).toBe(false)
-   expect(checkPlaceForShip(8, 0, 5, false)).toBe(true)
-})
